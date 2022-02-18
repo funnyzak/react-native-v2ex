26ced297ca9528b0c6d28a6eff2765f9671dc99d
leon <silenceace@gmail.com>
2022-02-18 20:23:16 +0800

    feat: logbox

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry, LogBox } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
+// Ignore log notification by message:
+// LogBox.ignoreLogs(['Warning: ...']);
+
+// Ignore all log notifications:
+// LogBox.ignoreAllLogs();
+
+AppRegistry.registerComponent(appName, () => App)
