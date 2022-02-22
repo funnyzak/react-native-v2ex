4a45be740fa414f17c285c21cf04a58929db24bf
leon <silenceace@gmail.com>
2022-02-22 21:12:41 +0800

    feat: components

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { View, ViewPropTypes, ActivityIndicator } from 'react-native'
+import PropTypes from 'prop-types'
+import { ThemeContext } from '../../theme'
+
+const Spinner = ({ size, style }) => {
+  const theme = useContext(ThemeContext)
+  return (
+    <View style={[styles.spinnerStyle, style]}>
+      <ActivityIndicator size={size} color={theme.colors.secondary} />
+    </View>
+  )
+}
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
+
+Spinner.propTypes = {
+  size: PropTypes.oneOf(['large', 'small']),
+  style: ViewPropTypes.style
+}
+
+Spinner.defaultProps = {
+  size: 'large',
+  style: {}
+}
+
+export { Spinner }
