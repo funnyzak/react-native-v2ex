6029df126b934d773c65e0c9013ece86274b03bb
leon <silenceace@gmail.com>
2022-02-23 21:39:59 +0800

    feat: action types

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,10 +1,10 @@
 /**
  * Created by leon<silenceace@gmail.com> on 22/2/22.
  */
-export const APP_INIT = 'app_init'
-export const APP_INIT_ERROR = 'app_init_error'
-export const APP_STORE_CONFIG = 'app_store_config'
-export const APP_LOCALES = 'app_locales'
+export const V2EX_INIT = 'v2ex_init'
+export const V2EX_INIT_ERROR = 'v2ex_init_error'
+export const V2EX_STORE_CONFIG = 'v2ex_store_config'
+export const V2EX_LOCALES = 'v2ex_locales'
 
 export const NOTIFICATION_LIST_PULL = 'notification_pull'
 export const NOTIFICATION_REMOVE = 'notification_pull'
@@ -20,9 +20,21 @@ export const TOPIC_REPLIES_PULL = 'topic_replies_pull'
 
 export const NAVIGATION_GO_TO_SCREEN = 'navigation_go_to_screen'
 
-export const APP_AUTH = 'app_auth'
+export const V2EX_AUTH = 'v2ex_auth'
 
-export const APP_LOGOUT = 'app_logout'
+export const V2EX_LOGOUT = 'v2ex_logout'
 
-export const APP_LOGIN_SUCCESS = 'app_login_success'
-export const APP_AUTH_ERROR = 'app_auth_error'
+export const V2EX_LOGIN_SUCCESS = 'v2ex_login_success'
+export const V2EX_AUTH_ERROR = 'v2ex_auth_error'
+
+export const V2EX_CURRNET_TOKEN = 'v2ex_current_token'
+export const V2EX_CURRNET_PROFILE = 'v2ex_current_profile'
+
+export interface StoreAction {
+  type: string
+  payload: any
+}
+
+export * from './../reducers/types'
+
+export * from './../v2ex/types'
