3ed6e6944fd0e3b578fe07561bcf281e6e6391c7
leon <silenceace@gmail.com>
2022-02-23 22:18:36 +0800

    feat: NavigationService

---
+++ src/navigation/NavigationService.ts
@@ -0,0 +1,29 @@
+import { NavigationActions, NavigationContainerComponent } from 'react-navigation'
+
+let navigator: NavigationContainerComponent
+
+function setTopLevelNavigator(navigatorRef: NavigationContainerComponent) {
+  navigator = navigatorRef
+}
+
+function navigate(routeName: string, params: Record<string, string>) {
+  navigator.dispatch(
+    NavigationActions.navigate({
+      routeName,
+      params
+    })
+  )
+}
+
+function goBack() {
+  //navigator._navigation.goBack();
+  navigator.dispatch(NavigationActions.back())
+}
+
+// add other navigation functions that you need and export them
+
+export default {
+  goBack,
+  navigate,
+  setTopLevelNavigator
+}
