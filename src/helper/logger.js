ea8940a7a789475405601df3a7bc27793a7289c8
leon <silenceace@gmail.com>
2022-02-23 16:32:44 +0800

    feat: add help logger

---
+++ src/helper/logger.js
@@ -0,0 +1,11 @@
+import { Client } from 'bugsnag-react-native'
+const bugsnag = new Client('f1eXXXXXXXXXXXf55776a5')
+
+export const logError = (error) => {
+  console.log(error)
+  if (error instanceof Error) {
+    bugsnag.notify(error)
+  } else if (typeof error === 'string') {
+    bugsnag.notify(new Error(error))
+  }
+}
