1ca50431150f5d2fb806bceba32251eae064670b
leon <silenceace@gmail.com>
2022-02-23 21:59:29 +0800

    feat: helper app

---
+++ src/helper/app.ts
@@ -0,0 +1,23 @@
+import AsyncStorage from '@react-native-community/async-storage'
+import { Store } from 'redux'
+import { v2ex } from '../v2ex'
+import { initV2ex, setCurrentToken } from '../actions'
+import { logError } from './logger'
+
+export const onAppStart = async (store: Store) => {
+  store.dispatch(initV2ex() as any)
+
+  const memberToken = await AsyncStorage.getItem('memberToken')
+
+  v2ex.setToken(memberToken === null ? undefined : memberToken)
+
+  if (memberToken) {
+    try {
+      const tokenDetail = await v2ex.member.getTokenDetail()
+      store.dispatch(setCurrentToken(tokenDetail.result))
+    } catch (error) {
+      console.log('onAppStart -> unable to retrieve current token', error)
+      logError(error)
+    }
+  }
+}
