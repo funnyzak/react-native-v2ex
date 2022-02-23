302088b44f606d5ac0d34589a8b4271dd06aac9b
leon <silenceace@gmail.com>
2022-02-23 21:40:23 +0800

    feat: memer action

--- src/actions/MemberActions.ts
+++ src/actions/MemberActions.ts
@@ -1,19 +1,24 @@
 /**
  * Created by leon<silenceace@gmail.com> on 22/2/22.
  */
+import { Dispatch } from 'redux'
+import { MEMBER_PROFILE_GET, MEMBER_TOKEN_GET, V2EX_CURRNET_TOKEN, IMemberTokenDetail } from './types'
 
-import { MEMBER_PROFILE_GET, MEMBER_TOKEN_GET } from './types'
-
-export const getProfile = () => async (dispatch: any) => {
+export const getProfile = () => async (dispatch: Dispatch) => {
   dispatch({
     type: MEMBER_PROFILE_GET,
     payload: {}
   })
 }
 
-export const getToken = () => async (dispatch: any) => {
+export const getToken = () => async (dispatch: Dispatch) => {
   dispatch({
     type: MEMBER_TOKEN_GET,
     payload: {}
   })
 }
+
+export const setCurrentToken = (token?: IMemberTokenDetail) => ({
+  type: V2EX_CURRNET_TOKEN,
+  payload: token
+})
