b6891bcb9823eb841f5dadab420eb7a34875b810
leon <silenceace@gmail.com>
2022-02-24 16:20:46 +0800

    feat: notification api

---
+++ src/v2ex/lib/notification/index.ts
@@ -0,0 +1,13 @@
+import { V2exAPI, V2exObject } from '../../types'
+
+export default (v2ex: V2exAPI.V2ex): V2exAPI.Notification => ({
+  /**
+   * Get my latest notifications
+   */
+  list: (page: number) => v2ex.get<V2exObject.Notification[]>(`/notifications?p=${page}`, undefined, undefined, 'v2'),
+
+  /**
+   * Remove notification
+   */
+  remove: (id: string) => v2ex.delete<any>(`/notifications/${id}`, undefined, 'v2')
+})
