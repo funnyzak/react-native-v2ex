077ec3ae06e1d7d72d0ef61cc435c83d2c93fe98
leon <silenceace@gmail.com>
2022-02-24 17:22:14 +0800

    chore: babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src'
+        }
+      }
+    ],
+    'react-native-reanimated/plugin',
+    '@babel/plugin-proposal-export-namespace-from'
+  ]
+}
