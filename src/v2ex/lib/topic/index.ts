6a733dc278e8e613e2170df9a76c177dc2796c70
leon <silenceace@gmail.com>
2022-02-24 16:20:01 +0800

    feat: topic api

---
+++ src/v2ex/lib/topic/index.ts
@@ -0,0 +1,32 @@
+import { V2exAPI, V2exObject } from '../../types'
+
+export default (v2ex: V2exAPI.V2ex): V2exAPI.Topic => ({
+  /**
+   *  Get node topic list
+   * @param name : node name
+   */
+  topicsByNode: (name = 'python') => v2ex.get<V2exObject.Topic[]>(`/nodes/${name}/topics`, undefined, undefined, 'v2'),
+
+  /**
+   * Get latest topic list
+   */
+  latestTopics: () => v2ex.get<V2exObject.Topic[]>('/topics/latest.json', undefined, undefined, undefined),
+  /**
+   * Get hot topic list
+   */
+  hotTopics: () => v2ex.get<V2exObject.Topic[]>('/topics/hot.json', undefined, undefined, undefined),
+  /**
+
+    /**
+     * Get topic info by topic id
+     * @param id : topic id
+     */
+  get: (id: string) => v2ex.get<V2exObject.Topic>(`/topics/${id}`, undefined, undefined, 'v2'),
+
+  /**
+   * Get topic replies
+   * @param topic_id : topic id
+   * @param page : page num
+   */
+  replies: (topic_id: string, page: number) => v2ex.get<V2exObject.TopicReplay[]>(`/topics/${topic_id}/replies?p=${page}`, undefined, undefined, 'v2')
+})
