1ad67380cbbeece9e893bbe99440a924ef20b50d
leon <silenceace@gmail.com>
2022-02-25 17:57:50 +0800

    feat:  persist transform setting

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,18 +4,29 @@ import thunk from 'redux-thunk'
 // https://github.com/rt2zz/redux-persist
 import { persistStore, persistReducer } from 'redux-persist'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
-import createFilter, { createBlacklistFilter } from 'redux-persist-transform-filter'
+import { createBlacklistFilter } from 'redux-persist-transform-filter'
 import AsyncStorage from '@react-native-community/async-storage'
 
 import reducers from '@src/reducers'
 import { window } from '@types'
 
-const accountSubsetBlacklistFilter = createBlacklistFilter('account', ['refreshing'])
+const memberSubsetBlacklistFilter = createBlacklistFilter('member', ['refreshing'])
 const uiSubsetBlacklistFilter = createBlacklistFilter('ui', ['refreshing'])
+const appSubsetBlacklistFilter = createBlacklistFilter('app', ['refreshing', 'errorMessage', 'latestVersion', 'v2ex'])
+const homeSubsetBlacklistFilter = createBlacklistFilter('home', ['refreshing'])
+const settingSubsetBlacklistFilter = createBlacklistFilter('setting', ['refreshing'])
+const notificationSubsetBlacklistFilter = createBlacklistFilter('notification', ['refreshing'])
 
 const persistConfig = {
   key: 'root',
-  transforms: [accountSubsetBlacklistFilter, uiSubsetBlacklistFilter],
+  transforms: [
+    memberSubsetBlacklistFilter,
+    uiSubsetBlacklistFilter,
+    appSubsetBlacklistFilter,
+    homeSubsetBlacklistFilter,
+    settingSubsetBlacklistFilter,
+    notificationSubsetBlacklistFilter
+  ],
   storage: AsyncStorage,
   stateReconciler: autoMergeLevel2
 }
