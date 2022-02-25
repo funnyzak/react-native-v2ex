b7da470d1df67ed5bdafa5867aa217c2abb71a6f
leon <silenceace@gmail.com>
2022-02-25 20:27:16 +0800

    feat: global declare

---
+++ global.d.ts
@@ -0,0 +1,23 @@
+declare module '*.png' {
+  import { ImageSourcePropType } from 'react-native'
+  const value: ImageSourcePropType
+  export default value
+}
+
+declare module '*.jpg' {
+  import { ImageSourcePropType } from 'react-native'
+  const value: ImageSourcePropType
+  export default value
+}
+
+declare module '*.jpeg' {
+  import { ImageSourcePropType } from 'react-native'
+  const value: ImageSourcePropType
+  export default value
+}
+
+declare module '*.gif' {
+  import { ImageSourcePropType } from 'react-native'
+  const value: ImageSourcePropType
+  export default value
+}
