ad6039df3f0037eaa3d991fc9b58e586509c8135
leon <silenceace@gmail.com>
2022-02-25 17:45:12 +0800

    feat: home tabs

---
+++ src/navigation/tabs.ts
@@ -0,0 +1,81 @@
+/**
+ * Created by leon<silenceace@gmail.com> on 22/2/25.
+ */
+
+import { IState } from '@types'
+
+export const NODE_TABS = {
+  LATEST: 'latest',
+  HOT: 'hot',
+  QNA: 'qna',
+  CITY: 'city',
+  DEALS: 'deals',
+  JOBS: 'jobs',
+  APPLE: 'apple',
+  PLAY: 'play',
+  CREATIVE: 'creative',
+  TECH: 'tech',
+  R2: 'R2',
+  NODES: 'nodes',
+  MEMBERS: 'members'
+}
+
+export type NODE_TABS = typeof NODE_TABS[keyof typeof NODE_TABS]
+
+export const HOME_NODES: Array<IState.NodeState> = [
+  {
+    name: NODE_TABS.LATEST,
+    title: '最新',
+    loginRequired: false
+  },
+  {
+    name: NODE_TABS.HOT,
+    title: '热门',
+    loginRequired: false
+  },
+  {
+    name: NODE_TABS.QNA,
+    title: '问与答',
+    loginRequired: true
+  },
+  {
+    name: NODE_TABS.CITY,
+    title: '城市',
+    loginRequired: true
+  },
+  {
+    name: NODE_TABS.DEALS,
+    title: '交易',
+    loginRequired: true
+  },
+  {
+    name: NODE_TABS.JOBS,
+    title: '酷工作',
+    loginRequired: true
+  },
+  {
+    name: NODE_TABS.APPLE,
+    title: 'Apple',
+    loginRequired: true
+  },
+  {
+    name: NODE_TABS.PLAY,
+    title: '好玩',
+    loginRequired: true
+  },
+  {
+    name: NODE_TABS.CREATIVE,
+    title: '创意',
+    loginRequired: true
+  },
+  {
+    name: NODE_TABS.TECH,
+    title: '技术',
+    loginRequired: true
+  },
+  {
+    name: NODE_TABS.R2,
+    title: 'R2',
+    loginRequired: true
+  }
+]
