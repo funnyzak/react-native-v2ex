9b6edc471b377e28e9819afd7c0bd4617118b907
leon <silenceace@gmail.com>
2022-02-25 13:48:55 +0800

    feat: define routers

--- src/navigation/routes.ts
+++ src/navigation/routes.ts
@@ -1,11 +1,24 @@
-export const NAVIGATION_HOME_SCREEN_PATH = 'HomeScreen'
-export const NAVIGATION_HOME_STACK_PATH = 'Home'
-export const NAVIGATION_ACCOUNT_PATH = 'Account'
-export const NAVIGATION_LOGIN_PATH = 'Login'
-export const NAVIGATION_SIGNIN_PATH = 'Signin'
-export const NAVIGATION_AUTH_STACK_PATH = 'Auth'
+export const NAVIGATION_HOME_PATH = 'home'
+
+export const NAVIGATION_LATEST_PATH = 'latest_topics'
+export const NAVIGATION_HOT_PATH = 'hot_topics'
+export const NAVIGATION_SIGNIN_PATH = 'sign_in'
+export const NAVIGATION_NOTIFICATION_PATH = 'notifications'
+
+export const NAVIGATION_NODE_LIST_PATH = 'node_list'
+export const NAVIGATION_NODE_TOPICS_PATH = 'node_topics'
+
+export const NAVIGATION_TOPIC_PATH = 'topic_detail'
+
+export const NAVIGATION_ACCOUNT_HOME_PATH = 'account_home'
+export const NAVIGATION_FOLLOW_PEOPLE_PATH = 'follow_people'
+export const NAVIGATION_LIKE_TOPICS_PATH = 'like_topics'
 export const NAVIGATION_AUTH_LOADING_SWITCH = 'navigation_auth_loading_switch'
-export const NAVIGATION_LOGIN_STACK_PATH = 'Login_Stack'
-export const NAVIGATION_ACCOUNT_STACK_PATH = 'Account_Stack'
-export const NAVIGATION_SEARCH_SCREEN_PATH = 'Search'
-export const BOTTOM_TAB_NAVIGATOR = 'Bottom_Tab_Navigator'
+
+export const NAVIGATION_SETTING_PATH = 'setting'
+export const NAVIGATION_LANGUAGE_PATH = 'language'
+export const NAVIGATION_ABOUT_PATH = 'about'
+export const NAVIGATION_FEEDBACK_PATH = 'feedback'
+export const NAVIGATION_THEME_PATH = 'theme'
+
+export const NAVIGATION_SEARCH_SCREEN_PATH = 'search'
