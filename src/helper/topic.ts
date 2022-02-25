0137cf33702ee5df46949c37a19ee03ec15faa4c
leon <silenceace@gmail.com>
2022-02-25 22:34:28 +0800

    feat: hook

---
+++ src/helper/topic.ts
@@ -0,0 +1,1 @@
+import { v2exLib } from '../v2ex'
---
+++ src/hooks/useTopic.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by leon<silenceace@gmail.com> on 22/2/25.
+ */
+import { useEffect, useState } from 'react'
+import { V2exObject } from '@types'
