a4d08d2664073c6393ede6dc0aa42cce209a8806
leon <silenceace@gmail.com>
2022-02-28 14:00:12 +0800

    fixed: notification reducer

--- src/reducers/NotificationReducer.ts
+++ src/reducers/NotificationReducer.ts
@@ -7,11 +7,11 @@ const INITIAL_STATE: IState.NotificationState = {
 export default (state: IState.NotificationState = INITIAL_STATE, action: Action): IState.NotificationState => {
   switch (action.type) {
     case APP_NOTIFICATION_PULL:
-      return { ..._state, list: undefined, refreshing: true }
+      return { ...state, list: undefined, refreshing: true }
     case APP_NOTIFICATION_LATEST:
-      return { ..._state, list: action.payload, refreshing: false }
+      return { ...state, list: action.payload, refreshing: false }
     case APP_NOTIFICATION_REMOVE:
-      return { ..._state, list: action.payload, refreshing: false }
+      return { ...state, list: action.payload, refreshing: false }
     default:
       return state
   }
