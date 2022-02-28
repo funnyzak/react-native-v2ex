1abb872ca26e1db5fe3bd2aefd44cd16d77d33b6
leon <silenceace@gmail.com>
2022-02-28 15:49:59 +0800

    feat: test

--- src/hooks/useTopic.ts
+++ src/hooks/useTopic.ts
@@ -9,13 +9,7 @@ export const useTopic = ({ topicId }: { topicId: string }) => {
   const [topic, setTopic] = useState<V2exObject.Topic | undefined>(undefined)
   const v2ex = useAppSelector((_state: any) => _state.app.v2ex as V2exAPI.V2ex)
 
-  useEffect(() => {
-    const fetchTopic = async () => {
-      const _topic = await v2ex.topic.get(topicId)
-      setTopic(_topic)
-    }
-    fetchTopic()
-  }, [topicId]) // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {}, [topicId])
 
   return {
     topic
