79aef6dea18a2667bec4dd5b0cf187541f88115b
leon <silenceace@gmail.com>
2022-03-02 17:13:19 +0800

    fixed: reducer

--- src/reducers/UIReducer.ts
+++ src/reducers/UIReducer.ts
@@ -1,4 +1,3 @@
-import _ from 'lodash'
 import { APP_AUTH_SUCCESS, FEEDBACKING, FEEDBACK_DONE, Action, IState, APP_AUTH_LOADING, APP_AUTH_ERROR, APP_LOGOUT } from '../types'
 import { translate } from '@src/i18n'
 
@@ -18,17 +17,17 @@ const INITIAL_STATE: IState.UIState = {
 export default (state: IState.UIState = INITIAL_STATE, action: Action): IState.UIState => {
   switch (action.type) {
     case APP_LOGOUT:
-      return { ...state, login: { ...state.login } }
+      return { ...state }
     case APP_AUTH_LOADING:
-      return _.merge(state, { login: { loading: true, error: '', success: '' } })
+      return { ...state, login: { ...state.login, loading: true, error: '', success: '' } }
     case APP_AUTH_ERROR:
-      return _.merge(state, { login: { loading: false, error: action.payload, success: '' } })
+      return { ...state, login: { ...state.login, loading: false, error: action.payload, success: '' } }
     case APP_AUTH_SUCCESS:
-      return _.merge(state, { login: { loading: false, error: '', success: translate('tips.授权成功') } })
+      return { ...state, login: { ...state.login, loading: false, error: '', success: translate('tips.authSuccess') } }
     case FEEDBACKING:
-      return _.merge(state, { feedback: { processing: true } })
+      return { ...state, feedback: { processing: true } }
     case FEEDBACK_DONE:
-      return _.merge(state, { feedback: { processing: false, message: undefined } })
+      return { ...state, feedback: { processing: false } }
     default:
       return state
   }
