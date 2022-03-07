88aabd88321bbb55d2a5e45a872c0a90c34fcdb4
leon <silenceace@gmail.com>
2022-03-07 18:02:12 +0800

    feat: topic action for home screen

--- src/actions/TopicActions.ts
+++ src/actions/TopicActions.ts
@@ -2,16 +2,88 @@
  * Created by leon<silenceace@gmail.com> on 22/2/22.
  */
 
-import { TOPIC_GET, TOPIC_REPLIES } from './types'
+import { Dispatch } from 'redux'
+import { v2exLib } from '@src/v2ex'
+import { logError } from '@src/helper/logger'
 
-export const topicGet = () => async (dispatch: any) => {
+import {
+  TOPIC_GET,
+  TOPIC_REPLIES,
+  APP_HOME_ERROR,
+  APP_HOME_REFRESH,
+  APP_HOME_LOAD_MORE_TOPICS,
+  APP_HOME_SWITCH_NODE,
+  APP_HOME_SUCCESS,
+  V2exObject
+} from '@src/types'
+import { NODE_TAB_TYPE } from '@src/navigation'
+import { SPECIAL_NODE_NAME_MAP } from '@src/config/constants'
+
+export const homeSwithTab = (tab: NODE_TAB_TYPE) => {
+  return async (dispath: Dispatch) => {
+    dispath({ type: APP_HOME_SWITCH_NODE, payload: tab })
+    dispath(homeTopicList(tab.name, 1) as any)
+  }
+}
+
+/**
+ * 获取首页主题列表
+ * @param nodeName 节点name
+ * @param page 获取页数
+ * @returns
+ */
+export const homeTopicList =
+  (nodeName: string, page: number = 1) =>
+  async (dispatch: Dispatch) => {
+    const specialNode = Object.values(SPECIAL_NODE_NAME_MAP).includes(nodeName)
+    const refreshing = page === 1 || specialNode
+    const loadmore = !refreshing && page > 1
+
+    if (refreshing) {
+      dispatch({
+        type: APP_HOME_REFRESH,
+        payload: {}
+      })
+    } else if (loadmore) {
+      dispatch({
+        type: APP_HOME_LOAD_MORE_TOPICS,
+        payload: {}
+      })
+    }
+
+    try {
+      let _topics: V2exObject.Topic[] = []
+      if (specialNode) {
+        if (nodeName === SPECIAL_NODE_NAME_MAP.HOT) {
+          _topics = await v2exLib.topic.hotTopics()
+        } else if (nodeName === SPECIAL_NODE_NAME_MAP.LATEST) {
+          _topics = await v2exLib.topic.latestTopics()
+        }
+      } else {
+        _topics = await v2exLib.topic.topicsByNode(nodeName, page)
+      }
+
+      dispatch({
+        type: APP_HOME_SUCCESS,
+        payload: _topics
+      })
+    } catch (error) {
+      logError(error)
+      dispatch({
+        type: APP_HOME_ERROR,
+        payload: error
+      })
+    }
+  }
+
+export const topicGet = (topicId: string) => async (dispatch: Dispatch) => {
   dispatch({
     type: TOPIC_GET,
     payload: {}
   })
 }
 
-export const topicReplies = () => async (dispatch: any) => {
+export const topicReplies = (topicId: string, pager: number) => async (dispatch: Dispatch) => {
   dispatch({
     type: TOPIC_REPLIES,
     payload: {}
