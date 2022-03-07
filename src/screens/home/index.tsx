4dbeb0a5f212ace853b5f2571183b810659e53a9
leon <silenceace@gmail.com>
2022-03-07 13:52:56 +0800

    feat: home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,45 @@
 import React, { useState } from 'react'
-import { StyleSheet, View, ViewStyle, TextStyle } from 'react-native'
+import { StyleSheet, View, ViewStyle, TextStyle, RefreshControl } from 'react-native'
 
 import { connect } from 'react-redux'
 import { IState, ITheme, V2exObject } from '@src/types'
-import { TopicTabList } from '../components'
-import { HomeScreenProps as ScreenProps, HOME_NODES, NODE_TAB_TYPE } from '@src/navigation'
+import { TopicTabList, TopicList } from '../components'
+import { HomeScreenProps as ScreenProps, HOME_NODES, NODE_TAB_TYPE, ROUTES } from '@src/navigation'
 
 const Home = ({ route, navigation, theme, loading }: ScreenProps) => {
+  const [currentTab, setCurrentTab] = useState<NODE_TAB_TYPE>(HOME_NODES[0])
+  const [canLoadMoreContent, setCanLoadMoreContent] = useState<boolean>(true)
+  const [topics, setTopics] = useState<V2exObject.Topic[] | undefined>(undefined)
+  const [searchIndicator, setSearchIndicator] = useState<boolean>(false)
+  const [refreshing, setRefreshing] = useState<boolean>(false)
+
+  const onEndReached = () => {
+    if (canLoadMoreContent) {
+    }
+  }
+
+  const onRefresh = () => {
+    setTopics(undefined)
+    setSearchIndicator(false)
+  }
+
   return (
     <View>
-      <TopicTabList />
+      <TopicTabList
+        tabs={HOME_NODES}
+        onPress={(tab: NODE_TAB_TYPE) => {
+          setCurrentTab(tab)
+        }}
+        currentTab={currentTab}
+      />
+      <TopicList
+        onRowPress={(topic: V2exObject.Topic) => {
+          navigation.navigate(ROUTES.TopicDetail, { topicId: topic.id.toString() })
+        }}
+        topics={topics}
+        onEndReached={onEndReached}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      />
     </View>
   )
 }
@@ -30,9 +60,9 @@ Home.defaultProps = {
   loading: false
 }
 
-const mapStateToProps = ({ ui: { login } }: { ui: IState.UIState }) => {
-  const { error, success, loading } = login
-  return { error, success, loading }
+const mapStateToProps = ({ ui: { home } }: { ui: IState.UIState }) => {
+  const { error, success, list, refreshing } = home
+  return { error, success, refreshing, list }
 }
 
 export default connect(mapStateToProps)(Home)
