3d6463f0b7b9352e90901daa2aa5a8cac0f8b3fa
leon <silenceace@gmail.com>
2022-03-07 18:00:30 +0800

    feat: constant define

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,9 +2,20 @@
  * Created by leon<silenceace@gmail.com> on 22/2/24.
  */
 
+/**
+ * @description 日期格式化规则
+ */
 export const DATE_FORMAT = 'DD/MM/YYYY'
 
 /**
  * @description 用户授权缓存key
  */
 export const MEMBER_TOKEN_KEY = 'memberToken'
+
+/**
+ * @description 特殊节点名称
+ */
+export const SPECIAL_NODE_NAME_MAP = {
+  LATEST: 'latest',
+  HOT: 'hot'
+}
