2512523770770fe8b7ac4cfbf4898e18a4c51106
leon <silenceace@gmail.com>
2022-03-14 16:24:46 +0800

    chore: eslint

---
+++ src/config/v2ex.ts
@@ -0,0 +1,23 @@
+/**
+ * v2ex Settings for the app,
+ *
+ * url                     : Base url of the v2ex website
+ * token            : Token to access v2ex API, without it
+ *                           app won't work
+ */
+export const v2exOptions = {
+  url: 'https://www.v2ex.com',
+  store: 'api',
+  userAgent: 'V2ex App API Library',
+  authentication: {
+    token: 'hello world'
+  }
+}
+
+export const aboutUs = {
+  author: 'leon',
+  email: 'y@yycc.me',
+  github: 'funnyzak',
+  wechat: 'funnyzak',
+  twitter: 'funnyzak'
+}
