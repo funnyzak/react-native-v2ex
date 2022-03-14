200a363dbbab1f58ed7fdb80dad186b087933be8
leon <silenceace@gmail.com>
2022-03-14 16:11:12 +0800

    feat: topic detail

--- src/screens/topic/Detail.tsx
+++ src/screens/topic/Detail.tsx
@@ -1,42 +1,85 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { StyleSheet, View, ViewStyle, TextStyle } from 'react-native'
+import { View, ViewStyle, TextStyle } from 'react-native'
+import RenderHtml from 'react-native-render-html'
 
 import { translate } from '@src/i18n'
 import { useTheme, SylCommon } from '@src/theme'
-import { IState, ITheme, V2exObject } from '@src/types'
-import * as CompS from '../components'
-import { Text, Spinner } from '@src/components'
+import { ITheme } from '@src/types'
+import { Text, Spinner, useToast, Avatar } from '@src/components'
 import { TopicDetailScreenProps as ScreenProps } from '@src/navigation/routes'
+import { useTopic } from '@src/hooks/useTopic'
+import dayjs from 'dayjs'
 
-const TopicDetail = ({ route, navigation, loading }: ScreenProps) => {
+const TopicDetail = ({ route, navigation }: ScreenProps) => {
   const { theme } = useTheme()
-  return (
-    <View style={[SylCommon.Layout.fill, SylCommon.View.background(theme)]}>
-      <Text>Hello, TopicDetail.</Text>
-    </View>
-  )
+  const { showMessage } = useToast()
+  const { topic } = useTopic({ topicId: route.params.topicId })
+
+  const reanderContent = () => {
+    if (!topic) {
+      return <Spinner style={{ marginTop: 50 }} />
+    }
+
+    navigation.setOptions({ title: topic.title })
+
+    return (
+      <>
+        <View style={styles.titleContainer(theme)}>
+          <Text type="heading">{topic.title}</Text>
+        </View>
+        <View style={styles.summary(theme)}>
+          <Avatar
+            username={topic.member?.username}
+            size={24}
+            source={topic.member ? { uri: topic.member?.avatar_normal } : undefined}
+          />
+          <Text style={styles.author(theme)}>{topic.member?.username}</Text>
+          <Text style={styles.attr(theme)}>{topic.created && dayjs(topic.created * 1000).fromNow()}</Text>
+          {topic.replies !== undefined && (
+            <Text style={styles.attr(theme)}>{`${topic.replies} ${translate('common.replies')}`}</Text>
+          )}
+          <Text style={SylCommon.Node.node(theme)}>{topic.node?.name}</Text>
+        </View>
+        <View style={SylCommon.Divider.item(theme)} />
+        <RenderHtml
+          source={{ html: topic.content || '<p></p>' }}
+          contentWidth={theme.dimens.WINDOW_WIDTH - theme.spacing.large * 2}
+        />
+      </>
+    )
+  }
+
+  return <View style={[SylCommon.Layout.fill, SylCommon.View.background(theme)]}>{reanderContent()}</View>
 }
 
 /**
  * @description styles settings
  */
 const styles = {
-  container: (theme: ITheme): ViewStyle => ({
-    flex: 1
+  titleContainer: (theme: ITheme): ViewStyle => ({
+    flexDirection: 'row'
+  }),
+  summary: (theme: ITheme): ViewStyle => ({
+    marginTop: theme.spacing.medium,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }),
+  author: (theme: ITheme): TextStyle => ({
+    marginHorizontal: 8,
+    fontSize: 14,
+    color: theme.colors.captionText
+  }),
+  attr: (theme: ITheme): TextStyle => ({
+    marginRight: 8,
+    fontSize: 12,
+    color: theme.colors.secondary
+  }),
+  replies: (theme: ITheme): TextStyle => ({
+    fontSize: 14,
+    color: theme.colors.secondary,
+    marginTop: 40
   })
 }
 
-/**
- * default props
- */
-TopicDetail.defaultProps = {
-  loading: false
-}
-
-const mapStateToProps = ({ ui: { login } }: { ui: IState.UIState }) => {
-  const { error, success, loading } = login
-  return { error, success, loading }
-}
-
-export default connect(mapStateToProps)(TopicDetail)
+export default connect()(TopicDetail)
