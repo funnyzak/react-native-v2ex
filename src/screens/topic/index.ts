a92a0f1d520bd69074861837e87b4676d785636d
leon <silenceace@gmail.com>
2022-03-15 19:30:27 +0800

    feat: remove hot latest screen

--- src/screens/topic/index.ts
+++ src/screens/topic/index.ts
@@ -1,5 +1,3 @@
 export { default as TopicDetailScreen } from './Detail'
-export { default as HotTopicListScreen } from './HotList'
-export { default as LatestTopicListScreen } from './LatestList'
 export { default as NodeTopicListScreen } from './NodeTopicList'
 export { default as HomeTabNodeTopicListScreen } from './HomeTabNodeTopicList'
