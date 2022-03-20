11d7cb0ff73a4ee731ef19b4b2bd4307f337226e
Leon <silenceace@gmail.com>
2022-03-20 23:00:11 +0800

    feat: node help

---
+++ src/helper/node.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by leon<silenceace@gmail.com> on 22/3/20.
+ */
+import { store } from '@src/store'
+import { V2exObject } from '@src/types'
+
+/**
+ * home tab nodes
+ */
+export interface TabNodeProps {
+  title: string
+  parentNodeNames: any[]
+  children?: V2exObject.Node[]
+}
+
+export let TabNodes: TabNodeProps[] = [
+  { title: 'V2EX', parentNodeNames: ['v2ex'] },
+  { title: 'Life', parentNodeNames: ['life'] },
+  { title: 'Geek', parentNodeNames: ['geek'] },
+  { title: 'Programming', parentNodeNames: ['programming'] },
+  { title: 'Earth', parentNodeNames: ['cn', 'us'] }
+]
+
+export const nodeChildren = (rootNode: TabNodeProps): V2exObject.Node[] => {
+  const { title, parentNodeNames: parentNodes } = rootNode
+
+  let nodes: V2exObject.Node[] = []
+
+  const all_node = store.getState().app.allNode
+  if (!all_node) return nodes
+
+  return all_node.filter((v) => parentNodes.includes(v.parent_node_name))
+}
