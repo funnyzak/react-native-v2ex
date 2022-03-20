160939c236cc40039710d0eea6f3559e42d6fbc3
Leon <silenceace@gmail.com>
2022-03-20 23:01:17 +0800

    feat: logbox

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { name as appName } from './app.json'
 import 'react-native-gesture-handler'
 
 // Ignore log notification by message:
-LogBox.ignoreLogs(['required dispatch_sync'])
+LogBox.ignoreLogs(['required dispatch_sync', 'flexWrap'])
 
 // Ignore all log notifications:
 // LogBox.ignoreAllLogs();
