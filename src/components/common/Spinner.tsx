dcb79aab3b902e6793adf01be9f42098e82483ed
leon <silenceace@gmail.com>
2022-03-31 21:01:01 +0800

    feat: spinner style

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,29 +1,43 @@
 import React from 'react'
-import { View, StyleProp, ViewStyle, ActivityIndicator } from 'react-native'
-import { useTheme } from '@src/theme'
+import { View, StyleProp, ViewStyle, ActivityIndicator, TextStyle } from 'react-native'
+import { Text } from './Text'
+import { ITheme, useTheme } from '@src/theme'
 
 const Spinner = ({
   size = 'large',
-  style
+  style,
+  text
 }: {
   size: number | 'small' | 'large' | undefined
   style: StyleProp<ViewStyle>
+  text?: string
 }) => {
   const { theme } = useTheme()
 
   return (
-    <View
-      style={[
-        {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        },
-        style
-      ]}>
+    <View style={[styles.containerStyle(theme), style]}>
       <ActivityIndicator size={size} color={theme.colors.secondary} />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text adjustsFontSizeToFit={true} style={styles.textStyle(theme)}>
+          {text}
+        </Text>
+      </View>
     </View>
   )
 }
 
 export { Spinner }
+
+const styles = {
+  containerStyle: (_theme: ITheme): ViewStyle => ({
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  }),
+  textStyle: (_theme: ITheme): TextStyle => ({
+    ..._theme.typography.captionText,
+    marginTop: _theme.spacing.small,
+    color: _theme.colors.secondary
+  })
+}
