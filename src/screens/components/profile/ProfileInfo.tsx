173ee1e1b9417d8f940755b6851bbcf07c446fea
leon <silenceace@gmail.com>
2022-04-06 18:39:02 +0800

    feat: profileinfo component

--- src/screens/components/profile/ProfileInfo.tsx
+++ src/screens/components/profile/ProfileInfo.tsx
@@ -10,6 +10,8 @@ import { ITheme, SylCommon, useTheme } from '@src/theme'
 import { translate } from '@src/i18n'
 import { NavigationService, ROUTES } from '@src/navigation'
 import { V2exObject } from '@src/types'
+import dayjs from 'dayjs'
+
 /**
  * // TODO: ProfileInfo
  * ProfileInfo props
@@ -24,12 +26,79 @@ const ProfileInfo: React.FC<ProfileInfoProps> = ({ profile }: ProfileInfoProps) 
 
   const renderContent = () => {
     return (
-      <TouchableOpacity style={[SylCommon.Layout.fullWidth, SylCommon.Layout.row, styles.baseInfo(theme)]}>
-        <Avatar source={profile?.avatar_normal ? { uri: profile?.avatar_normal } : undefined} size={60} />
-        <View style={styles.userInfo(theme)}>
-          <Text style={styles.username(theme)}>{profile?.username}</Text>
+      <View style={styles.container(theme)}>
+        <View style={styles.infoItem(theme)}>
+          <View style={styles.baseAvatar(theme)}>
+            <Avatar
+              size={60}
+              source={profile?.avatar_normal ? { uri: profile?.avatar_normal } : undefined}
+              username={profile?.username}
+            />
+          </View>
+          <View style={styles.baseRightBox(theme)}>
+            <Text
+              style={[styles.baseRightItem(theme), theme.typography.subheadingText, { color: theme.colors.secondary }]}>
+              {profile?.username}
+            </Text>
+            {profile?.tagline && (
+              <Text style={[styles.baseRightItem(theme), theme.typography.bodyText]}>{profile?.tagline}</Text>
+            )}
+            {profile?.last_modified && (
+              <Text style={[styles.baseRightItem(theme), theme.typography.captionText]}>
+                {translate('label.activeLatest').replace(
+                  '$',
+                  dayjs(profile?.last_modified * 1000).format('YYYY-MM-DD HH:mm:ss')
+                )}
+              </Text>
+            )}
+          </View>
         </View>
-      </TouchableOpacity>
+        {profile?.bio && <Text style={[styles.infoItem(theme), theme.typography.bodyText]}>{profile?.bio}</Text>}
+        <View style={styles.infoItem(theme)}>
+          {profile?.location && (
+            <TextWithIconPress
+              style={{ marginRight: theme.spacing.small }}
+              text={profile?.location}
+              icon={theme.assets.images.icons.profile.location}
+            />
+          )}
+          {profile?.website && (
+            <TextWithIconPress
+              style={{ marginRight: theme.spacing.small }}
+              text={profile?.website}
+              icon={theme.assets.images.icons.profile.urlschme}
+            />
+          )}
+        </View>
+        <View style={styles.infoItem(theme)}>
+          {profile?.github && (
+            <TextWithIconPress
+              style={{ marginRight: theme.spacing.small }}
+              text={profile?.github}
+              icon={theme.assets.images.icons.profile.github}
+            />
+          )}
+          {profile?.telegram && (
+            <TextWithIconPress
+              style={{ marginRight: theme.spacing.small }}
+              text={profile?.telegram}
+              icon={theme.assets.images.icons.profile.telegram}
+            />
+          )}
+          {profile?.twitter && (
+            <TextWithIconPress
+              style={{ marginRight: theme.spacing.small }}
+              text={profile?.twitter}
+              icon={theme.assets.images.icons.profile.twitter}
+            />
+          )}
+        </View>
+        {profile?.created && (
+          <Text style={[styles.infoItem(theme), theme.typography.captionText]}>
+            {translate('label.joinV2exSinceTime').replace('$', dayjs(profile?.created * 1000).format())}
+          </Text>
+        )}
+      </View>
     )
   }
 
@@ -38,16 +107,17 @@ const ProfileInfo: React.FC<ProfileInfoProps> = ({ profile }: ProfileInfoProps) 
 
 const styles = {
   container: (theme: ITheme): ViewStyle => ({
-    flex: 1,
-    backgroundColor: theme.colors.surface
+    backgroundColor: theme.colors.surface,
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column'
   }),
   infoItem: (theme: ITheme): ViewStyle => ({
-    paddingBottom: theme.spacing.medium
-  }),
-  baseInfo: (theme: ITheme): ViewStyle => ({
+    paddingBottom: theme.spacing.medium,
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start'
   }),
   baseAvatar: (theme: ITheme): ViewStyle => ({
     width: 60,
@@ -61,16 +131,8 @@ const styles = {
     flexGrow: 1,
     alignItems: 'flex-start'
   }),
-
-  userInfo: (theme: ITheme): ViewStyle => ({
-    marginLeft: 12,
-    flex: 1,
-    justifyContent: 'center'
-  }),
-  username: (theme: ITheme): TextStyle => ({
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 4
+  baseRightItem: (theme: ITheme): ViewStyle => ({
+    paddingBottom: theme.spacing.small
   })
 }
 
