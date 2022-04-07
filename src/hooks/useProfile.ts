355b3f504011ec36fe1a1920741201a708bd7964
Leon <silenceace@gmail.com>
2022-04-07 23:17:16 +0800

    feat: useprofile

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,28 @@
+/**
+ * Created by leon<silenceace@gmail.com> on 22/4/7.
+ */
+import { useEffect, useState } from 'react'
+import { V2exObject, IState } from '../types'
+import { useAppSelector } from '.'
+
+export const useProfile = ({ username }: { username: string | number }) => {
+  const [member, setMember] = useState<V2exObject.Member | undefined>(undefined)
+  const v2ex = useAppSelector((_state: IState.State) => _state.app.v2ex)
+
+  useEffect(() => {
+    const fetchMember = async () => {
+      const _member = await v2ex?.member.profile(username)
+      if (_member) {
+        setMember(_member)
+      }
+    }
+
+    if (username) {
+      fetchMember()
+    }
+  }, [username, v2ex])
+
+  return {
+    profile: member
+  }
+}
