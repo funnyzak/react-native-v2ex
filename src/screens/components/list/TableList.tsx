0de1959f6969f627762f050c3968bfd30aec6693
leon <silenceace@gmail.com>
2022-04-11 16:54:30 +0800

    feat: component table list

--- src/screens/components/list/TableList.tsx
+++ src/screens/components/list/TableList.tsx
@@ -2,7 +2,16 @@
  * Created by leon<silenceace@gmail.com> on 22/04/01.
  */
 import React from 'react'
-import { View, ViewStyle, TextStyle, StyleProp, ImageSourcePropType, ImageStyle, Image } from 'react-native'
+import {
+  View,
+  ViewStyle,
+  TextStyle,
+  StyleProp,
+  ImageSourcePropType,
+  ImageStyle,
+  Image,
+  TouchableOpacity
+} from 'react-native'
 
 import { Text, Button, Spinner, Placeholder } from '@src/components'
 import { ITheme, SylCommon, useTheme } from '@src/theme'
@@ -47,26 +56,26 @@ export interface TableRowProps {
   /**
    * press callback
    */
-  onPress: () => void
+  onPress?: () => void
 }
 
 const TableRow: React.FC<TableRowProps> = (data: TableRowProps) => {
   const { theme } = useTheme()
 
   return (
-    <View style={[data.containerStyle, rowStyles.container(theme)]}>
+    <TouchableOpacity onPress={data.onPress} style={[data.containerStyle, rowStyles.container(theme)]}>
       <View style={rowStyles.left(theme)}>
         {data.icon && <Image source={data.icon} style={rowStyles.icon(theme)} />}
         <View style={rowStyles.textBox(theme)}>
           <Text style={rowStyles.title(theme)}>{data.title}</Text>
           {data.description && <Text style={rowStyles.description(theme)}>{data.description}</Text>}
         </View>
-        <View style={rowStyles.right(theme)}>
-          {data.rightText && <Text style={rowStyles.rightText(theme)}>{data.rightText}</Text>}
-          <Image source={theme.assets.images.icons.table.rightArrow} style={rowStyles.arrow(theme)} />
-        </View>
       </View>
-    </View>
+      <View style={rowStyles.right(theme)}>
+        {data.rightText && <Text style={rowStyles.rightText(theme)}>{data.rightText}</Text>}
+        <Image source={theme.assets.images.icons.table.rightArrow} style={rowStyles.arrow(theme)} />
+      </View>
+    </TouchableOpacity>
   )
 }
 
@@ -84,10 +93,7 @@ export interface TableListProps {
    */
   title?: string
 
-  /**
-   * Table row list
-   */
-  list?: Array<TableRowProps>
+  children?: React.ReactNode
 }
 
 const TableList: React.FC<TableListProps> = (data: TableListProps) => {
@@ -95,10 +101,8 @@ const TableList: React.FC<TableListProps> = (data: TableListProps) => {
   const renderContent = () => {
     return (
       <View style={[tableStyles.container(theme), data.containerStyle]}>
-        <Text style={tableStyles.title(theme)}>Hello World, TableList.</Text>
-        <View style={tableStyles.list(theme)}>
-          {data.list && data.list.map((item, index) => <TableRow key={index} {...item} />)}
-        </View>
+        <Text style={tableStyles.title(theme)}>{data.title}</Text>
+        <View style={tableStyles.list(theme)}>{data.children}</View>
       </View>
     )
   }
@@ -108,24 +112,68 @@ const TableList: React.FC<TableListProps> = (data: TableListProps) => {
 
 const rowStyles = {
   container: (theme: ITheme): ViewStyle => ({
-    flex: 1
+    display: 'flex',
+    flexDirection: 'row',
+    height: 40,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 0.3,
+    borderColor: theme.colors.border
   }),
-  left: (theme: ITheme): ViewStyle => ({}),
-  icon: (theme: ITheme): ImageStyle => ({}),
-  textBox: (theme: ITheme): ViewStyle => ({}),
-  title: (theme: ITheme): TextStyle => ({}),
+  left: (theme: ITheme): ViewStyle => ({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  }),
+  icon: (theme: ITheme): ImageStyle => ({
+    height: 21,
+    width: 21,
+    marginRight: theme.spacing.small
+  }),
+  textBox: (theme: ITheme): ViewStyle => ({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  }),
+  title: (theme: ITheme): TextStyle => ({
+    ...theme.typography.labelText
+  }),
   description: (theme: ITheme): TextStyle => ({}),
-  right: (theme: ITheme): ViewStyle => ({}),
-  rightText: (theme: ITheme): TextStyle => ({}),
-  arrow: (theme: ITheme): ImageStyle => ({})
+  right: (theme: ITheme): ViewStyle => ({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: theme.spacing.medium
+  }),
+  rightText: (theme: ITheme): TextStyle => ({
+    ...theme.typography.captionText
+  }),
+  arrow: (theme: ITheme): ImageStyle => ({
+    width: 16,
+    height: 16
+  })
 }
 
 const tableStyles = {
   container: (theme: ITheme): ViewStyle => ({
-    flex: 1
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginTop: theme.spacing.extraLarge
   }),
-  title: (theme: ITheme): TextStyle => ({}),
-  list: (theme: ITheme): ViewStyle => ({})
+  title: (theme: ITheme): TextStyle => ({
+    ...theme.typography.captionText,
+    paddingLeft: theme.spacing.medium,
+    marginBottom: theme.spacing.small
+  }),
+  list: (theme: ITheme): ViewStyle => ({
+    display: 'flex',
+    paddingLeft: theme.spacing.medium,
+    flexDirection: 'column',
+    backgroundColor: theme.colors.surface,
+    width: '100%'
+  })
 }
 
 export { TableList, TableRow }
