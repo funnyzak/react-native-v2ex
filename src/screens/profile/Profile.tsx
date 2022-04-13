7bb12000af5ef3ec3a1977ecf355270c7240ad86
leon <silenceace@gmail.com>
2022-04-13 12:22:36 +0800

    chore: yarn、ios

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -1,19 +1,25 @@
 import { logout as actionLogout } from '@src/actions'
+import { Spinner, Text } from '@src/components'
 import { useProfile } from '@src/hooks/useProfile'
-import { ProfileScreenProps as ScreenProps } from '@src/navigation/routes'
-import { SylCommon, useTheme } from '@src/theme'
-import { ITheme } from '@src/types'
-import { Spinner } from '@src/components'
-import React, { useEffect, useMemo, useLayoutEffect } from 'react'
-import { View, ViewStyle } from 'react-native'
-import { connect } from 'react-redux'
-import { ProfileInfo, HeaderButton } from '../components'
-import { linking } from '@src/utils'
 import { translate } from '@src/i18n'
+import { ProfileScreenProps as ScreenProps, ROUTES } from '@src/navigation'
+import { SylCommon, useTheme } from '@src/theme'
+import { IState, ITheme, V2exObject } from '@src/types'
+import React, { useEffect, useLayoutEffect, useMemo } from 'react'
+import { TouchableOpacity, View, ViewStyle } from 'react-native'
+import { connect } from 'react-redux'
+import { HeaderButton, ProfileInfo } from '../components'
 
-const Profile = ({ route, navigation }: ScreenProps) => {
+const Profile = ({
+  route,
+  navigation,
+  authMember,
+  logout
+}: ScreenProps & {
+  authMember?: V2exObject.Member
+  logout: () => void
+}) => {
   const { theme } = useTheme()
-  const [loading, setLoading] = React.useState(true)
   const username = useMemo(() => route.params.username, [route])
   const { profile } = useProfile({ username: username })
 
@@ -24,14 +30,24 @@ const Profile = ({ route, navigation }: ScreenProps) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: profile
-        ? () => (
-            <HeaderButton
-              source={theme.assets.images.icons.header.link}
-              onPress={() => {
-                linking(profile?.url)
-              }}
-            />
-          )
+        ? () =>
+            !authMember || authMember.id !== profile.id ? (
+              <HeaderButton
+                text={translate('common.follow')}
+                onPress={() => {
+                  // TODO: 关注
+                }}
+              />
+            ) : (
+              <HeaderButton
+                source={theme.assets.images.icons.header.logout}
+                onPress={() => {
+                  // TODO: 退出确认
+                  logout()
+                  navigation.navigate(ROUTES.SignIn)
+                }}
+              />
+            )
         : undefined
     })
   }, [navigation, profile])
@@ -57,6 +73,12 @@ const styles = {
     paddingTop: theme.spacing.medium
   })
 }
-export default connect(undefined, {
+
+const mapStateToProps = ({ member }: { member: IState.MemberState }) => {
+  const { profile: authMember } = member
+  return { authMember }
+}
+
+export default connect(mapStateToProps, {
   logout: actionLogout
 })(Profile)
