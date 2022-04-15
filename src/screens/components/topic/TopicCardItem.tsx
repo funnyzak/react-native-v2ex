fe7440f30c7b2ee159e6efbe4643f9b472a1093e
leon <silenceace@gmail.com>
2022-04-15 21:52:35 +0800

    feat: topic card and list component

---
+++ src/screens/components/topic/TopicCardItem.tsx
@@ -0,0 +1,166 @@
+/**
+ * Created by leon<silenceace@gmail.com> on 22/04/15.
+ */
+
+import * as React from 'react'
+import { View, TouchableOpacity, ViewStyle, TextStyle } from 'react-native'
+import { ITheme, V2exObject } from '@src/types'
+import { useTheme } from '@src/theme'
+import { Avatar, Text } from '@src/components'
+import dayjs from 'dayjs'
+import { NavigationService, ROUTES } from '@src/navigation'
+import { BorderLine, TextWithIconPress } from '../common'
+import { useMemo } from 'react'
+import { translate } from '@src/i18n'
+
+export interface TopicCardItemProps {
+  /**
+   * container style
+   */
+  containerStyle?: ViewStyle
+
+  /**
+   * Display Style
+   */
+  displayStyle?: 'simple' | 'full' | 'auto'
+
+  /**
+   * Topic Info
+   */
+  topic: V2exObject.Topic
+
+  /**
+   * Topic Title Press Event
+   */
+  onPress?: (topic: V2exObject.Topic) => void
+}
+
+const TopicCardItem = ({ containerStyle, displayStyle = 'auto', topic, onPress }: TopicCardItemProps) => {
+  const { theme } = useTheme()
+
+  const display_style = useMemo(
+    () =>
+      ['auto', 'full'].includes(displayStyle) && topic.member && (topic.member.avatar || topic.member.avatar_normal)
+        ? 'full'
+        : 'simple',
+    [displayStyle]
+  )
+
+  const avatar_link = useMemo(
+    () => (topic.member ? topic.member.avatar || topic.member.avatar_normal : undefined),
+    [topic]
+  )
+
+  return (
+    <View style={[styles.container(theme), containerStyle]}>
+      <View style={styles.infoContainer(theme)}>
+        {display_style === 'full' && <Avatar size={40} source={{ uri: avatar_link }} style={styles.avatar(theme)} />}
+        <View style={styles.infoMain(theme)}>
+          {display_style === 'full' && (
+            <View style={styles.infoMainItem(theme)}>
+              <TextWithIconPress
+                text={topic.member?.username ?? 'none'}
+                textStyle={[{ color: theme.colors.secondary }]}
+                onPress={() => {
+                  NavigationService.navigate(ROUTES.Profile, { username: topic.member?.username })
+                }}
+              />
+              <TextWithIconPress
+                text={translate('label.postedTime').replace('$', dayjs(topic.created * 1000).fromNow())}
+              />
+            </View>
+          )}
+          <TouchableOpacity
+            style={styles.infoMainItem(theme)}
+            onPress={() => {
+              onPress && onPress(topic)
+              NavigationService.navigate(ROUTES.TopicDetail, { topic_id: topic.id })
+            }}>
+            <Text type="body" style={styles.title(theme)}>
+              {topic.title}
+            </Text>
+          </TouchableOpacity>
+
+          <View style={styles.infoMainItem(theme)}>
+            <View style={styles.summaryContainer(theme)}>
+              {topic.last_reply_by !== '' && (
+                <TextWithIconPress
+                  containerStyle={[{ marginRight: theme.spacing.small }]}
+                  text={topic.last_reply_by}
+                  icon={theme.assets.images.icons.topic.talk}
+                  textStyle={[{ color: theme.colors.secondary }]}
+                  onPress={() => {
+                    NavigationService.navigate(ROUTES.Profile, { username: topic.last_reply_by })
+                  }}
+                />
+              )}
+              <TextWithIconPress
+                containerStyle={[{ marginRight: theme.spacing.small }]}
+                text={topic.replies.toString()}
+                icon={theme.assets.images.icons.topic.comment}
+              />
+              <TextWithIconPress
+                text={dayjs(topic.last_touched * 1000).fromNow()}
+                icon={theme.assets.images.icons.topic.time}
+              />
+            </View>
+            {display_style === 'full' && topic.node ? (
+              <TextWithIconPress
+                text={topic.node?.title}
+                onPress={() => {
+                  NavigationService.navigate(ROUTES.NodeTopics, {
+                    nodeName: topic.node?.name,
+                    nodeTitle: topic.node?.title
+                  })
+                }}
+                icon={theme.assets.images.icons.topic.paper}
+                textStyle={[{ color: theme.colors.secondary }]}
+              />
+            ) : (
+              <TextWithIconPress text={dayjs(topic.created * 1000).fromNow()} />
+            )}
+          </View>
+        </View>
+      </View>
+      <BorderLine />
+    </View>
+  )
+}
+
+const styles = {
+  container: (theme: ITheme): ViewStyle => ({
+    marginTop: theme.spacing.tiny,
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: theme.colors.surface
+  }),
+  infoContainer: (theme: ITheme): ViewStyle => ({
+    flexDirection: 'row',
+    marginBottom: theme.spacing.medium
+  }),
+  avatar: (theme: ITheme): ViewStyle => ({
+    width: 40,
+    marginRight: theme.spacing.large
+  }),
+  infoMain: (theme: ITheme): ViewStyle => ({
+    flex: 1,
+    justifyContent: 'flex-start',
+    flexDirection: 'column'
+  }),
+  infoMainItem: (theme: ITheme): ViewStyle => ({
+    flexDirection: 'row',
+    marginBottom: theme.spacing.small,
+    justifyContent: 'space-between'
+  }),
+  summaryContainer: (theme: ITheme): ViewStyle => ({
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
+  }),
+  title: (theme: ITheme): TextStyle => ({
+    ...theme.typography.bodyText
+  })
+}
+
+export default TopicCardItem
--- src/screens/components/topic/TopicCardList.tsx
+++ src/screens/components/topic/TopicCardList.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { View, Text, FlatList, ViewStyle, TextStyle } from 'react-native'
-import { ITheme, V2exObject } from '@src/types'
-import { TopicItem, NotFound } from '../'
-import { Spinner } from '@src/components'
-import { SylCommon, useTheme } from '@src/theme'
+import { Placeholder, Spinner } from '@src/components'
 import { translate } from '@src/i18n'
 import { NavigationService, ROUTES } from '@src/navigation'
+import { SylCommon, useTheme } from '@src/theme'
+import { ITheme, V2exObject } from '@src/types'
+import React from 'react'
+import { FlatList, View, ViewStyle } from 'react-native'
+import TopicCardItem from './TopicCardItem'
 
-export interface TopicListProps {
+export interface TopicCardListProps {
   onRowPress?: (topic: V2exObject.Topic) => void
   canLoadMoreContent?: boolean
   topics?: Array<V2exObject.Topic>
@@ -17,7 +17,7 @@ export interface TopicListProps {
   refreshCallback?: () => void
 }
 
-const TopicList: React.FC<TopicListProps> = ({
+const TopicCardList: React.FC<TopicCardListProps> = ({
   onRowPress,
   canLoadMoreContent,
   topics,
@@ -25,7 +25,7 @@ const TopicList: React.FC<TopicListProps> = ({
   refreshControl,
   searchIndicator,
   refreshCallback
-}: TopicListProps) => {
+}: TopicCardListProps) => {
   const { theme } = useTheme()
 
   const onItemPress = (topic: V2exObject.Topic) => {
@@ -35,18 +35,14 @@ const TopicList: React.FC<TopicListProps> = ({
 
   const renderItemRow = ({ item }: { item: V2exObject.Topic }) =>
     !item || item === null ? null : (
-      <TopicItem containerStyle={styles.topicItemContainer(theme)} topic={item} onRowPress={onItemPress} />
+      <TopicCardItem containerStyle={styles.topicItemContainer(theme)} topic={item} onPress={onItemPress} />
     )
 
   const renderFooter = () => {
     if (canLoadMoreContent) {
       return <Spinner style={{ padding: theme.spacing.large }} />
     } else if (topics && topics.length > 0) {
-      return (
-        <View style={styles.notFoundTextWrap()}>
-          <Text style={styles.notFoundText(theme)}>{translate('tips.noMore')}</Text>
-        </View>
-      )
+      return <Placeholder placeholderText={translate('tips.noMore')} />
     }
     return null
   }
@@ -61,8 +57,7 @@ const TopicList: React.FC<TopicListProps> = ({
     if (topics.length > 0) {
       return (
         <FlatList
-          style={{ backgroundColor: theme.colors.surface }}
-          contentContainerStyle={{ flexGrow: 1 }}
+          style={[SylCommon.Card.container(theme), { paddingTop: theme.spacing.tiny }]}
           refreshControl={refreshControl}
           data={topics}
           renderItem={renderItemRow}
@@ -79,9 +74,9 @@ const TopicList: React.FC<TopicListProps> = ({
     }
     if (!searchIndicator) {
       return (
-        <NotFound
-          text={translate('placeholder.noTopics')}
-          buttonText={translate('button.oneceAgain')}
+        <Placeholder
+          placeholderText={translate('placeholder.noTopics')}
+          buttonText={translate('button.tryAgain')}
           buttonPress={refreshCallback}
         />
       )
@@ -100,29 +95,8 @@ const styles = {
   }),
   topicItemContainer: (theme: ITheme): ViewStyle => ({}),
   itemSeparator: (theme: ITheme) => ({
-    height: theme.spacing.small
-  }),
-  imageStyle: (theme: ITheme) => ({
-    height: 60,
-    width: undefined,
-    margin: theme.spacing.small,
-    borderWidth: 1,
-    borderColor: theme.colors.border
-  }),
-  notFoundTextWrap: (): TextStyle => ({
-    flex: 1,
-    paddingVertical: 20,
-    justifyContent: 'center'
-  }),
-  notFoundText: (theme: ITheme): TextStyle => ({
-    ...theme.typography.bodyText,
-    textAlign: 'center'
-  }),
-  separator: (theme: ITheme) => ({
-    width: 1,
-    backgroundColor: theme.colors.border,
-    marginVertical: theme.spacing.small
+    height: theme.spacing.tiny
   })
 }
 
-export default TopicList
+export default TopicCardList
