7c497f267e0fc4179d5ad25a2cf84b2be1fc0e30
leon <silenceace@gmail.com>
2022-04-19 15:35:55 +0800

    feat: sitestat screen

--- src/screens/setting/SiteStat.tsx
+++ src/screens/setting/SiteStat.tsx
@@ -1,36 +1,38 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import { View, ViewStyle, TouchableOpacity } from 'react-native'
-
-import * as Actions from '@src/actions'
 import { translate } from '@src/i18n'
-import { useTheme, SylCommon } from '@src/theme'
-import { IState, ITheme, V2exObject } from '@src/types'
-import * as CompS from '../components'
-import { Text, Spinner } from '@src/components'
 import { SiteStatScreenProps as ScreenProps } from '@src/navigation/routes'
+import { SylCommon, useTheme } from '@src/theme'
+import { IState, V2exObject } from '@src/types'
+import React from 'react'
+import { View } from 'react-native'
+import { connect } from 'react-redux'
+import { TableList, TableRow } from '../components'
 
-const SiteStat = ({ route, navigation, loading }: ScreenProps) => {
+const SiteStat = ({
+  siteStat
+}: ScreenProps & {
+  siteStat?: V2exObject.SiteStat
+}) => {
   const { theme } = useTheme()
   return (
-    <View style={[SylCommon.Layout.fill, SylCommon.View.background(theme)]}>
-      <Text>Hello, SiteStat.</Text>
+    <View style={SylCommon.Layout.fill}>
+      <TableList containerStyle={[{ marginTop: theme.spacing.tiny }]}>
+        <TableRow
+          title={translate('label.registedMember')}
+          value={siteStat?.member_max.toString() ?? 'None'}
+          withArrow={false}
+        />
+        <TableRow
+          title={translate('label.topicCount')}
+          value={siteStat?.topic_max.toString() ?? 'None'}
+          withArrow={false}
+        />
+      </TableList>
     </View>
   )
 }
 
-/**
- * @description styles settings
- */
-const styles = {
-  container: (theme: ITheme): ViewStyle => ({
-    flex: 1
-  })
-}
-
-const mapStateToProps = ({ ui: { login } }: { ui: IState.UIState }) => {
-  const { error, success, loading } = login
-  return { error, success, loading }
+const mapStateToProps = ({ app: { siteStat } }: { app: IState.AppState }) => {
+  return { siteStat }
 }
 
 export default connect(mapStateToProps)(SiteStat)
