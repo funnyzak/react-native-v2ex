6e1796b6fb2b65b6607fdd9903ee547089a88c72
leon <silenceace@gmail.com>
2022-04-19 20:17:28 +0800

    feat: add member、node to cache action

--- src/actions/CacheAction.ts
+++ src/actions/CacheAction.ts
@@ -1,3 +1,5 @@
+import { logError } from '@src/helper/logger'
+import { v2exLib } from '@src/v2ex'
 import { Dispatch } from 'redux'
 
 import {
@@ -7,6 +9,30 @@ import {
   APP_CACHE_RESET_MEMBERS,
   APP_CACHE_RESET_NODES
 } from '../types'
+
+export const addMember = (userid: string | number) => async (dispatch: Dispatch) => {
+  try {
+    const member = await v2exLib.member.profile(userid)
+    dispatch({
+      type: APP_CACHE_ADD_MEMBER,
+      payload: member
+    })
+  } catch (e: any) {
+    logError(e)
+  }
+}
+
+export const addNode = (nodeid: string | number) => async (dispatch: Dispatch) => {
+  try {
+    const node = await v2exLib.node.get(nodeid, undefined)
+    dispatch({
+      type: APP_CACHE_ADD_NODE,
+      payload: node
+    })
+  } catch (e: any) {
+    logError(e)
+  }
+}
 
 export const resetCache = (type: 'all' | 'nodes' | 'members') => ({
   type: type === 'nodes' ? APP_CACHE_RESET_NODES : type === 'members' ? APP_CACHE_RESET_MEMBERS : APP_CACHE_RESET
