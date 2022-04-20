4e9e89d8851e4f75ca4c5e5e95ec8a78a11a5a28
leon <silenceace@gmail.com>
2022-04-20 11:15:43 +0800

    feat: node detail screen

--- src/screens/node/NodeDetail.tsx
+++ src/screens/node/NodeDetail.tsx
@@ -1,12 +1,13 @@
 import { useNode } from '@src/hooks/useNode'
 import { NodeDetailScreenProps as ScreenProps } from '@src/navigation/routes'
-import { SylCommon } from '@src/theme'
+import { SylCommon, useTheme } from '@src/theme'
 import React, { useEffect } from 'react'
 import { View } from 'react-native'
 import { connect } from 'react-redux'
-import { NodeInfoCard } from '../components'
+import { NodeInfoCard, NodeTopicTabList } from '../components'
 
 const NodeDetail = ({ route, navigation }: ScreenProps) => {
+  const { theme } = useTheme()
   useEffect(() => {
     navigation.setOptions({
       title: route.params.nodeTitle
@@ -16,6 +17,7 @@ const NodeDetail = ({ route, navigation }: ScreenProps) => {
   return (
     <View style={SylCommon.Layout.fill}>
       <NodeInfoCard nodeid={route.params.nodeName} />
+      <NodeTopicTabList nodename={route.params.nodeName} containerStyle={[{ marginTop: theme.spacing.small }]} />
     </View>
   )
 }
