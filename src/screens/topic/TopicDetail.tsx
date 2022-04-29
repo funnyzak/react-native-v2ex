c6c8bc4cf5ae12d65025b1f0fe951e305bd22060
leon <silenceace@gmail.com>
2022-04-29 13:31:28 +0800

    feat: add star button to topic detail screen

--- src/screens/topic/TopicDetail.tsx
+++ src/screens/topic/TopicDetail.tsx
@@ -1,20 +1,37 @@
 /**
  * Created by leon<silenceace@gmail.com> on 22/04/28.
  */
-import { Spinner } from '@src/components'
+import { Spinner, useToast } from '@src/components'
 import { useTopic } from '@src/hooks/useTopic'
 import { translate } from '@src/i18n'
 import { ROUTES, TopicDetailScreenProps as ScreenProps } from '@src/navigation'
 import { SylCommon, useTheme } from '@src/theme'
 import React, { useEffect } from 'react'
 import { ScrollView, View } from 'react-native'
-import { SetStatusBar, TopicInfo, TopicReplay } from '../components'
+import { HeaderButton, SetStatusBar, TopicInfo, TopicReplay } from '../components'
 const TopicDetail = ({ route, navigation }: ScreenProps) => {
   const { theme } = useTheme()
   const { topic } = useTopic({ topicId: route.params.topicId })
+  const { showMessage } = useToast()
+
+  const underConstruction = () => {
+    showMessage({
+      type: 'error',
+      text2: translate('label.underConstruction')
+    })
+  }
 
   useEffect(() => {
-    navigation.setOptions({ title: !topic ? translate(`router.${ROUTES.TopicDetail}`) : `${topic.node?.title}` })
+    navigation.setOptions({
+      title: !topic ? translate(`router.${ROUTES.TopicDetail}`) : `${topic.node?.title}`,
+      headerRight: () => {
+        return (
+          <>
+            <HeaderButton source={theme.assets.images.icons.header.star} onPress={underConstruction} />
+          </>
+        )
+      }
+    })
   }, [topic, navigation])
 
   const reanderContent = () => {
