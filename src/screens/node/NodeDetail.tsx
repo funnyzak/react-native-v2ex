fd11e222ceb613be5b37a5c670954906e0e0865c
leon <silenceace@gmail.com>
2022-05-05 17:31:24 +0800

    feat: add interest header button to node screen

--- src/screens/node/NodeDetail.tsx
+++ src/screens/node/NodeDetail.tsx
@@ -1,5 +1,7 @@
+/**
+ * Created by leon<silenceace@gmail.com> on 22/04/30.
+ */
 import { interestNode, unInterestNode } from '@src/actions'
-import { translate } from '@src/i18n'
 import { NodeDetailScreenProps as ScreenProps } from '@src/navigation/routes'
 import { RootState } from '@src/store'
 import { SylCommon, useTheme } from '@src/theme'
@@ -7,17 +9,14 @@ import { V2exObject } from '@src/types'
 import React, { useEffect, useMemo, useState } from 'react'
 import { View } from 'react-native'
 import { connect } from 'react-redux'
-import { HeaderButton, NodeInfoCard, NodeTopicTabList } from '../components'
+import { NodeInfoCard, NodeTopicTabList } from '../components'
+import { LikeNodeHeaderButton } from '../components/button'
 
 const NodeDetail = ({
-  interest,
-  unInterest,
   interestNodes,
   route,
   navigation
 }: ScreenProps & {
-  interest: (node: V2exObject.Node) => void
-  unInterest: (node: V2exObject.Node) => void
   interestNodes: V2exObject.Node[]
 }) => {
   const { theme } = useTheme()
@@ -25,26 +24,7 @@ const NodeDetail = ({
   const [info, setInfo] = useState<V2exObject.Node | undefined>(undefined)
 
   const HeaderRight = () => () => {
-    return (
-      <>
-        {!info ? undefined : interestNodes.find((node) => node.name === nodeName) ? (
-          <HeaderButton
-            text={translate('common.unFollow')}
-            textColor={theme.colors.captionText}
-            onPress={() => {
-              unInterest(info)
-            }}
-          />
-        ) : (
-          <HeaderButton
-            text={translate('common.follow')}
-            onPress={() => {
-              interest(info)
-            }}
-          />
-        )}
-      </>
-    )
+    return !info ? undefined : <LikeNodeHeaderButton node={info} />
   }
 
   useEffect(() => {
@@ -52,7 +32,7 @@ const NodeDetail = ({
       title: route.params.nodeTitle,
       headerRight: HeaderRight()
     })
-  }, [interestNodes])
+  }, [interestNodes, info])
 
   return (
     <View style={SylCommon.Layout.fill}>
@@ -66,7 +46,4 @@ const mapStateToProps = ({ member: { interestNodes } }: RootState) => {
   return { interestNodes }
 }
 
-export default connect(mapStateToProps, {
-  interest: interestNode,
-  unInterest: unInterestNode
-})(NodeDetail)
+export default connect(mapStateToProps, {})(NodeDetail)
