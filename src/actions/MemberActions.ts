e898be2877809e98c8d18ce38034fd52eafef095
leon <silenceace@gmail.com>
2022-05-27 20:39:34 +0800

    feat: member action

--- src/actions/MemberActions.ts
+++ src/actions/MemberActions.ts
@@ -19,9 +19,13 @@ import {
   MEMBER_SATE_SETTING,
   MEMBER_TOPICS,
   MEMBER_UNINTEREST_NODE,
+  MEMBER_FOLLOW_PEOPLE,
+  MEMBER_UNFOLLOW_PEOPLE,
+  MEMBER_LIKE_TOPICS,
+  MEMBER_UNLIKE_TOPICS,
   V2exObject
 } from '../types'
-import { cacheMemberInterestNodes } from './CacheAction'
+import { cacheMemberFollowing, cacheMemberInterestNodes, cacheMemberLikeTopicss } from './CacheAction'
 
 export const myProfile = () => async (dispatch: Dispatch, getState: () => RootState) => {
   const _member = await v2exLib.member.myProfile()
@@ -66,6 +70,38 @@ export const unInterestNode = (node: V2exObject.Node) => async (dispatch: Dispat
     payload: node
   })
   dispatch(cacheMemberInterestNodes(getState().member.interestNodes))
+}
+
+export const likeTopic = (topic: V2exObject.Topic) => async (dispatch: Dispatch, getState: () => RootState) => {
+  dispatch({
+    type: MEMBER_LIKE_TOPICS,
+    payload: topic
+  })
+  dispatch(cacheMemberLikeTopicss(getState().member.likeTopics))
+}
+
+export const unLikeTopic = (topic: V2exObject.Topic) => async (dispatch: Dispatch, getState: () => RootState) => {
+  dispatch({
+    type: MEMBER_UNLIKE_TOPICS,
+    payload: topic
+  })
+  dispatch(cacheMemberLikeTopicss(getState().member.likeTopics))
+}
+
+export const followPeople = (member: V2exObject.Member) => async (dispatch: Dispatch, getState: () => RootState) => {
+  dispatch({
+    type: MEMBER_FOLLOW_PEOPLE,
+    payload: member
+  })
+  dispatch(cacheMemberFollowing(getState().member.followPeoples))
+}
+
+export const unFollowPeople = (member: V2exObject.Member) => async (dispatch: Dispatch, getState: () => RootState) => {
+  dispatch({
+    type: MEMBER_UNFOLLOW_PEOPLE,
+    payload: member
+  })
+  dispatch(cacheMemberFollowing(getState().member.followPeoples))
 }
 
 export const setCurrentToken = (token?: V2exObject.MToken) => ({
