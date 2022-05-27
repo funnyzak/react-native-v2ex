c4183dfc85df591a010b41c05f8f060a6037fbb3
leon <silenceace@gmail.com>
2022-05-27 20:39:55 +0800

    feat: type

--- src/screens/components/button/header/index.ts
+++ src/screens/components/button/header/index.ts
@@ -1,2 +1,3 @@
 export { default as LikeNodeHeaderButton } from './LikeNode'
 export { default as LikeTopicHeaderButton } from './LikeTopic'
+export { default as FollowPeopleHeaderButton } from './FollowPeople'
