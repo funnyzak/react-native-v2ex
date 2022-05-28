2b75fefada187dbd78ff7bc8a56016719fb0a33a
Leon <silenceace@gmail.com>
2022-05-28 23:05:21 +0800

    feat: add confirm sheet to logout action

---
+++ src/screens/components/actionsheet/index.ts
@@ -0,0 +1,4 @@
+/**
+ * Created by leon<silenceace@gmail.com> on 22/05/28.
+ */
+export { default as ConfirmActionSheet } from './ConfirmActionSheet'
--- src/screens/components/button/header/LogoutHeaderButton.tsx
+++ src/screens/components/button/header/LogoutHeaderButton.tsx
@@ -4,11 +4,14 @@
 import { logout as actionLogout } from '@src/actions'
 import { useAppDispatch } from '@src/hooks'
 import { useSession } from '@src/hooks/useSession'
+import { translate } from '@src/i18n'
 import { NavigationService, ROUTES } from '@src/navigation'
 import { useTheme } from '@src/theme'
 import { V2exObject } from '@src/types'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleProp, ViewStyle } from 'react-native'
+import { SheetManager } from 'react-native-actions-sheet'
+import { ConfirmActionSheet } from '../../actionsheet'
 import { HeaderButton } from '../../common'
 
 /**
@@ -27,23 +30,32 @@ const LogoutHeaderButton = ({
   containerStyle?: StyleProp<ViewStyle>
   member: V2exObject.Member
 }) => {
+  const [confirmSheetId] = useState<string>('action_logout')
   const { theme } = useTheme()
-  const { logined, profile } = useSession()
+  const { profile } = useSession()
   const dispatch = useAppDispatch()
 
-  const buttonPress = () => {
-    if (logined) {
-      dispatch(actionLogout() as any)
-      NavigationService.navigate(ROUTES.SignIn)
-    }
-  }
-
   return member && profile && member.id === profile.id ? (
-    <HeaderButton
-      source={theme.assets.images.icons.header.logout}
-      onPress={buttonPress}
-      containerStyle={containerStyle}
-    />
+    <>
+      <HeaderButton
+        source={theme.assets.images.icons.header.logout}
+        onPress={() => SheetManager.show(confirmSheetId)}
+        containerStyle={containerStyle}
+      />
+      <ConfirmActionSheet
+        sheetId={confirmSheetId}
+        title={translate('brand.name')}
+        text={translate('confirm.logout')}
+        confirmText={translate('common.confirm')}
+        cancelText={translate('common.cancel')}
+        confirmAction={(yes: boolean) => {
+          if (yes) {
+            dispatch(actionLogout() as any)
+            NavigationService.navigate(ROUTES.SignIn)
+          }
+        }}
+      />
+    </>
   ) : null
 }
 
