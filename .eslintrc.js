cc3c0a51f2098fadab4c4bd742c8237a35a3b68b
leon <silenceace@gmail.com>
2022-11-23 23:14:23 +0800

    fix: "jest/globals" is unknow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['react', 'react-native', '@typescript-eslint'],
+  plugins: ['jest', 'react', 'react-native', '@typescript-eslint'],
   extends: ['@react-native-community', 'prettier'],
   globals: {
     window: true
